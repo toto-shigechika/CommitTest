6c4b061196d77dfc383b7cc11fd341ac1944aac0
toto-shigechika <shigechika@touto-systems.com>
2024-12-20 16:12:28 +0900

    Revert "Add: Hello World 4"
    This reverts commit a1deeaeafac33f5b5e10aeffb5a40512035d0886.

--- src/App.java
+++ src/App.java
@@ -3,6 +3,5 @@ public class App {
         System.out.println("Hello, World!");
         System.out.println("Hello, World!2");
         System.out.println("Hello, World!3");
-        System.out.println("Hello, World!4");
     }
 }
