fd786d9c87bd4662d44bf0889a5f5e0530193b37
toto-shigechika <shigechika@touto-systems.com>
2024-12-20 16:15:16 +0900

    Del: App2の内部処理

--- src/App2.java
+++ src/App2.java
@@ -1,7 +1,4 @@
 public class App2 {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!2");
-        System.out.println("Hello, World!3");
     }
 }
