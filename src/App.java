88e91c7b711eec9b9edc49affdbfc97c709a6e0c
toto-shigechika <shigechika@touto-systems.com>
2024-12-20 16:24:00 +0900

    Update: Change 4.5

--- src/App.java
+++ src/App.java
@@ -3,6 +3,6 @@ public class App {
         System.out.println("Hello, World!");
         System.out.println("Hello, World!2");
         System.out.println("Hello, World!3");
-        System.out.println("Hello, World!4");
+        System.out.println("Hello, World!4.5");
     }
 }
