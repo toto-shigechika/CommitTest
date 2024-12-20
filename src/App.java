e53573df78368452a1281284f2e4e84cf18898c9
toto-shigechika <shigechika@touto-systems.com>
2024-12-20 16:24:23 +0900

    Update: printlnをprintに変更

--- src/App.java
+++ src/App.java
@@ -1,8 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!2");
-        System.out.println("Hello, World!3");
-        System.out.println("Hello, World!4.5");
+        System.out.print("Hello, World!");
+        System.out.print("Hello, World!2");
+        System.out.print("Hello, World!3");
+        System.out.print("Hello, World!4.5");
     }
 }
