cf136e97e41367682a8a899c1763b74991a559a5
toto-shigechika <shigechika@touto-systems.com>
2024-12-20 16:14:42 +0900

    Add: App2.java

--- src/App2.java
+++ src/App2.java
@@ -1,4 +1,7 @@
 public class App2 {
     public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!2");
+        System.out.println("Hello, World!3");
     }
 }
